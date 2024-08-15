c031513569aa3b0376428fc53deba23ec5382ac6
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-08-15 19:19:29 +0600

    added search and sort bar

--- src/page/Home/AllProducts/AllProducts.jsx
+++ src/page/Home/AllProducts/AllProducts.jsx
@@ -3,96 +3,132 @@ import ProductCard from "../../../components/Navbar/ProductCard/ProductCard";
 import Sidebar from "../../../components/Navbar/Sidebar/Sidebar";
 import axios from "axios";
 
-
 const AllProducts = () => {
-    const [products,setProducts] = useState([]);
-    const [count,setCount]=useState(0)
-    const [itemsPerPage,setItemsPerPage]=useState(9);
-    const [currentPage,setCurrentPage]=useState(1);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(9);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const numberOfPages = Math.ceil(count/itemsPerPage);
+  const [search, setSearch] = useState("");
+  const [searchText, setSearchText] = useState("");
 
-    useEffect(()=>{
-        getData();
-    },[currentPage, itemsPerPage,])
+  const numberOfPages = Math.ceil(count / itemsPerPage);
 
-    const getData = async()=>{
-        const {data} = await axios(`${import.meta.env.VITE_API_URL}/all-products`,{withCredentials:true})
-        // console.log(data);
-        setProducts(data);
-    }
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios(
+        `${
+          import.meta.env.VITE_API_URL
+        }/all-products?page=${currentPage}&size=${itemsPerPage}&search=${search}`,
+        { withCredentials: true }
+      );
 
-    useEffect(()=>{
-        const getData = async()=>{
-            const {data} = await axios(`${import.meta.env.VITE_API_URL}/all-products?page=${currentPage}&size=${itemsPerPage}`,{withCredentials:true})
+      setProducts(data);
+    };
+    getData();
+  }, [currentPage, itemsPerPage, search]);
 
-            setProducts(data); 
-        }
-        getData();
-    },[currentPage, itemsPerPage])
+  // get count
+  useEffect(() => {
+    const getCount = async () => {
+      const { data } = await axios(
+        `${import.meta.env.VITE_API_URL}/products-count?&search=${search}`,
+        { withCredentials: true }
+      );
+      setCount(data.count);
+      setLoading(false);
+    };
 
-    // get count
-    useEffect(()=>{
-        const getCount = async()=>{
-            const {data} = await axios(`${import.meta.env.VITE_API_URL}/products-count?`,{withCredentials:true})
-            setCount(data.count)
-            setLoading(false)
-        }
+    getCount();
+  }, [search]);
+  // console.log(count);
 
-        getCount();
-    },[])
-    // console.log(count);
+  const pages = [...Array(numberOfPages).keys()].map((element) => element + 1);
 
+  const handlePaginationButton = (value) => {
+    // console.log(value);
+    setCurrentPage(value);
+    window.scrollTo(0, 0);
+  };
 
-    const pages = [...Array(numberOfPages).keys()].map(element=>element+1)
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(searchText);
+  };
+  return (
+    <div className="container mx-auto flex gap-6">
+      {/* sidebar */}
+      <Sidebar />
 
-    const handlePaginationButton = (value)=>{
-        // console.log(value);
-        setCurrentPage(value);
-        window.scrollTo(0, 0);
-    }
-    return (
-        <div className="container mx-auto flex gap-6">
-            {/* sidebar */}
-            <Sidebar/>
-            
-            <div>
-            <div className="border border-[#aeffde] h-[80px] w-full">
+      <div>
 
+        <div className="w-full flex justify-between">
+            {/* search bar */}
+          <form class="flex flex-col md:flex-row gap-3"  onSubmit={handleSearch}>
+            <div class="flex">
+              <input
+                type="text"
+                placeholder="Search product name"
+                class="w-full md:w-80 px-3 h-10 rounded-l border-2 border-[#aeffde] focus:outline-none focus:border-[#aeffde]"
+              />
+              <button
+                type="submit"
+                class="bg-[#2e302f] text-white rounded-r px-2 md:px-3 py-0 md:py-1"
+              >
+                Search
+              </button>
             </div>
-            <div className="grid grid-cols-3 gap-3">
-            {
-                products.map(product=><ProductCard key={product._id} product={product}/>)
-            }
-            </div>
-            {/* pagination */}
-            <div className="my-12 flex justify-center items-center">
-            
-            {/* prev button */}
-            <button
-            disabled={currentPage === 1}
-            onClick={()=>handlePaginationButton(currentPage-1)}
-            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-white rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-green-500  hover:text-white">Prev</button>
+          </form>
 
-            {
-                pages.map(pageNum=>(
-                    <button
-                    onClick={()=>handlePaginationButton(pageNum)}
-                    className={`hidden ${currentPage === pageNum? 'bg-green-500 text-white': ''} px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-green-500  hover:text-white`} 
-                    key={{pageNum}}>{pageNum}</button>
-                ))
-            }
-
-            {/* next button */}
-            <button
-            disabled={currentPage === numberOfPages}
-            onClick={()=>handlePaginationButton(currentPage+1)}
-            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-white rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-green-500  hover:text-white">next</button>
-            </div>
-            </div>
-            
+          {/* sort by */}
+          <select id="pricingType" name="pricingType"
+		class="w-[100px] h-10 border-2 border-[#aeffde] focus:outline-none focus:border-[#aeffde] text-[#222] rounded px-2 md:px-3 py-0 md:py-1 tracking-wider">
+		<option value="All" selected="">All</option>
+		<option value="Freemium">Freemium</option>
+		<option value="Free">Free</option>
+		<option value="Paid">Paid</option>
+	</select>
         </div>
-    );
+        <div className="grid grid-cols-3 gap-3">
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+        {/* pagination */}
+        <div className="my-12 flex justify-center items-center">
+          {/* prev button */}
+          <button
+            disabled={currentPage === 1}
+            onClick={() => handlePaginationButton(currentPage - 1)}
+            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-white rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-[#aeffde]  hover:text-[#222]"
+          >
+            Prev
+          </button>
+
+          {pages.map((pageNum) => (
+            <button
+              onClick={() => handlePaginationButton(pageNum)}
+              className={`hidden ${
+                currentPage === pageNum ? "bg-[#aeffde] text-[#222]" : ""
+              } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-[#aeffde]  hover:text-[#222]`}
+              key={{ pageNum }}
+            >
+              {pageNum}
+            </button>
+          ))}
+
+          {/* next button */}
+          <button
+            disabled={currentPage === numberOfPages}
+            onClick={() => handlePaginationButton(currentPage + 1)}
+            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-white rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-[#aeffde]  hover:text-white"
+          >
+            next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllProducts;
